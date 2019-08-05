e7025af458fc7a74170905d2ba5a2aad1602ec96
Sergey Borodachev <borodachev@mercurydevelopment.com>
2019-08-06 01:38:22 +0400

    Integration with Azure pipeline + unit tests.

---
+++ tests/Magic.Spec.Tests/SpecTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Magic.Specs;
+using Xunit;
+
+namespace Magic.Spec.Tests
+{
+    public class SpecTests
+    {
+        [Fact]
+        public void SpecSimpleTest()
+        {
+            var notNullSpec = new Spec<object>(x => x != null);
+
+            notNullSpec.IsSatisfiedBy(new object()).Should().BeTrue();
+            notNullSpec.IsSatisfiedBy(null).Should().BeFalse();
+        }
+    }
+}
